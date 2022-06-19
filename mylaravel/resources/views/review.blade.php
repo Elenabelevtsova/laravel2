962b3914964079b8f558770975e5d0844f8065b0
Elenabelevtsova <lenabelevcova426@gmail.com>
2022-06-19 13:29:48 +0300

    first commit

---
+++ mylaravel/resources/views/review.blade.php
@@ -0,0 +1,36 @@
+@extends('layout')
+
+@section('title')Отзывы@endsection
+
+@section('main_content')
+
+@if($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+<h1>Form for review</h1>
+
+<form method="post" action="/review/check">
+    @csrf
+    <input type="email" name="email" id="email" placeholder="Enter email" class="form-control"><br>
+    <input type="text" name="subject" id="subject" placeholder="Enter review" class="form-control"><br>
+    <textarea name="message" id="message" class="form-control" placeholder="Enter message"></textarea><br>
+    <button type="submit" class="btn btn-success">Send</button>
+</form>
+<br>
+<h1>All reviews</h1>
+@foreach($reviews as $el)
+<div class="alert alert-warning">
+    <h3>{{ $el->subject }}</h3>
+    <p>{{ $el->created_at }}</p>
+    <b>{{ $el->email }}</b>
+    <p>{{ $el->message }}</p>
+</div>
+@endforeach
+
+@endsection
